e9c81629df2955a10d312ab156e5bb6918aa8a45
torn6810 <torn6810@gmail.com>
2021-11-24 19:02:54 -0800

    Put a space after Open-source code link

--- src/Footer.js
+++ src/Footer.js
@@ -3,13 +3,9 @@ import React from "react";
 export default function Footer(){
     return(
         <div className="Footer">
-        <a 
-        href="https://github.com/torn6810/react-weather-app" 
-        target="_blank"
-        rel="noreferrer"> 
-        Open Source Code
-        </a> 
-        by Edith Tornel
+        <a href="https://github.com/torn6810/react-weather-app" target="_blank" rel="noreferrer"> Open-source code </a> 
+
+         by Edith Tornel
         </div>
     );
 }
